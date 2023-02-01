a8764c1836a27730f2a46b2c910da56d71747914
cong <congco@foxmail.com>
2023-02-01 16:35:00 +0800

    feat:init

---
+++ src/main/java/com/cong/filecompare/FileCompareApplication.java
@@ -0,0 +1,13 @@
+package com.cong.filecompare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileCompareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileCompareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cong/filecompare/FileCompareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cong.filecompare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileCompareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
