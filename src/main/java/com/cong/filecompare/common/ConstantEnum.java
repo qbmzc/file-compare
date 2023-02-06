c18770cc6ef4d175cc8505a3a0f74b88f61a6bba
cong.zheng <congco@foxmail.com>
2023-02-06 11:16:36 +0800

    feat:comment

--- src/main/java/com/cong/filecompare/common/ConstantEnum.java
+++ src/main/java/com/cong/filecompare/common/ConstantEnum.java
@@ -1,6 +1,7 @@
 package com.cong.filecompare.common;
 
 /**
+ *  // Enum ConstantEnum is used to store constant values. It contains a single constant X-TOKEN with its value.
  * @author cong
  * @date 2023/2/1 17:23
  */
--- src/main/java/com/cong/filecompare/common/MapperEnum.java
+++ src/main/java/com/cong/filecompare/common/MapperEnum.java
@@ -1,15 +1,20 @@
-package com.cong.devola.config;
-
+package com.cong.filecompare.common;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+/**
+ *  This is an enum class used to create a singleton instance of an ObjectMapper. It provides a getter method to access the ObjectMapper instance.
+ */
 public enum MapperEnum {
     INSTANCE;
 
     private final ObjectMapper objectMapper;
 
-    MapperEnum() {
-        objectMapper = new ObjectMapper();
+    private MapperEnum() {
+        objectMapper= new ObjectMapper();
     }
+
+
+
 
     public ObjectMapper getObjectMapper() {
         return objectMapper;
